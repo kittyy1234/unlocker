b47ed1815d1a9cae40259fc23e42fb641435d78a
kittyy1234 <Theajhilton@icloud.com>
2026-08-27 16:58:20 -0400

    Update main.swift

--- main.swift
+++ main.swift
@@ -1,25 +1,13 @@
-import Foundation
-import CoreGraphics
+#!/bin/bash
 
-@_silgen_name("CGVirtualDisplayCreate")
-func CGVirtualDisplayCreate(_ properties: CFDictionary) -> Int32
+osascript -e '
+tell application "System Events"
+    try
+        set displayConfig to {Width:1920, Height:1080, RefreshRate:360, IsVirtual:true, MirrorMaster:true}
+    end try
+end tell
+' 2>/dev/null || true
 
-func bootCustomEngine() {
-    let displaySettings: [String: Any] = [
-        "Width": 1920,
-        "Height": 1080,
-        "RefreshRate": 360,
-        "IsVirtual": true,
-        "MirrorMaster": true
-    ]
-    
-    let resultStatus = CGVirtualDisplayCreate(displaySettings as CFDictionary)
-    
-    if resultStatus == 0 {
-        RunLoop.main.run()
-    } else {
-        exit(1)
-    }
-}
-
-bootCustomEngine()
+while true; do
+    sleep 3600
+done
