5baf17e817abbf30f6a5f3ec8e2aaceadb02500c
kittyy1234 <Theajhilton@icloud.com>
2026-08-29 22:33:18 -0400

    Delete uncap_fps.sh

--- uncap_fps.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-printf '#!/bin/bash\nexec "/Applications/Roblox.app/Contents/MacOS/RobloxPlayer" --disable-vsync --force-gles-renderer\n' > launch_unlocked.sh
-chmod +x launch_unlocked.sh
-
-./launch_unlocked.sh
